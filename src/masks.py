a8376a0a6ffce3e480de90e4f3b7d08d981b4a75
Kseniya Rip <ksucha_18_94@mail.ru>
2025-09-07 19:53:01 +0300

    init commit

--- src/masks.py
+++ src/masks.py
@@ -1,11 +1,24 @@
-def get_mask_card_number(card_number: str) -> str:
+def get_mask_card_number(number: str) -> str:
     """
-    Функция, которая принимает число - номер карты
-    Маска, которая преобразует число в формат XXXX XX** **** XXXX
+    Функция принимает число - номер карты
+    Маска
+    преобразует число в формат XXXX XX** **** XXXX
     """
-    masked_number = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+    masked_number = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
     return masked_number
 
 
 card_number: str = "7000792289606361"
 print(get_mask_card_number(card_number))
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    Функция принимает номер карты
+    Маска преобразует номер карты в формат **XXXX
+    """
+    return f"**{account_number[-4:]}"
+
+
+card_tipe_number = "73654108430135874305"
+print(get_mask_account(card_tipe_number))
--- src/widget.py
+++ src/widget.py
@@ -1,26 +1,27 @@
-def mask_account_card(card_tipe_number: str) -> str:
+from src.masks import get_mask_card_number, get_mask_account, card_number
+
+
+def mask_account_card(number: str) -> str:
     """
-       Функция, которая принимает тип и число - тип и номер карты, тип и номер расчетного счета
-       Маска, которая преобразует тип и число в формат тип XXXX XX** **** XXXX или в **XXXX
+       Функция принимает тип и число - тип и номер карты, тип и номер расчетного счета
+       Маска преобразует тип и число в формат тип XXXX XX** **** XXXX или в **XXXX
        """
-    card_info = card_tipe_number.split(' ', 1)
-    if 'счет' in card_info[0].lower():
-        masked_account = f"{card_info[0]} **{card_info[1][-4:]}"
-        return masked_account
+    card_info, number = number.rsplit(' ', maxsplit=1)
+    if 'счет' in card_info.lower():
+        masked_number = get_mask_card_number(card_number)
     else:
-        masked_number = f"{card_info[0]} {card_info[1][:4]} {card_info[1][4:6]}** **** {card_info[1][-4:]}"
-        return masked_number
+        masked_number = get_mask_account(card_tipe_number)
+
+    return f'{card_info} {masked_number}'
 
 
 card_tipe_number = "Visa Platinum 7000792289606361"
 print(mask_account_card(card_tipe_number))
 
+
 def get_date(date_tipe: str) -> str:
     """
-       Функция, которая принимает строку с датой в формате "2024-03-11T02:26:18.67140
-       Функция, которая возвращает строку с датой в формате "ДД.ММ.ГГГГ"
+       Функция принимает строку с датой в формате "2024-03-11T02:26:18.67140
+       Функция возвращает строку с датой в формате "ДД.ММ.ГГГГ"
     """
     return f'{date_tipe[8:10]}.{date_tipe[5:7]}.{date_tipe[:4]}'
-
-date_tipe: str = "2024-03-11T02:26:18.67140"
-print(get_date(date_tipe))
